97de8ea235ae57d6fdbb9df5c9d1dfa1c040df68
Lara-Codes <110344853+Lara-Codes@users.noreply.github.com>
2023-08-09 10:57:00 -0400

    Functioning with 1 button, 3 states

--- pseudoCode.ino
+++ pseudoCode.ino
@@ -1,82 +1,116 @@
+// Include for boolean operations 
 #include <stdbool.h>
-#include <LiquidCrystal.h>
+
+// Includes for the LED display 
+#include <Wire.h> // Enable this line if using Arduino Uno, Mega, etc.
+#include <Adafruit_GFX.h>
+#include "Adafruit_LEDBackpack.h"
 
 // Pins connected to each of the 2 sensors 
-const int sensorOnePin = 1; 
-const int sensorTwoPin = 2; 
-const int buttonPin = 3; // connected to "reset timer" button
+const int sensorOnePin = 2; 
+const int sensorTwoPin = 16;
 
-// Storing times each sensor has been triggered 
-unsigned long startTime = 0; 
-unsigned long endTime = 0; 
-unsigned long elapsedTime = 0; 
+// LED display 
+Adafruit_7segment matrix = Adafruit_7segment();
 
-// Check if the first sensor has been triggered  
-bool firstSensorTriggered = false; 
+// Indicates if you want the dots to be displayed on the LED board (ex. 11:11)
+bool drawDots; 
 
-// Is the timer running or stopped? 
-bool timerRunning = false; 
+// Each digit on LED display 
+int dig0, dig1, dig3, dig4; 
 
-// Connect digital timing clock 
-LiquidCrystal lcd(4, 5, 6, 7, 8); // numbers of interface pins of digital display 
+int state;
 
-void setup(){
-    // Sensor 1 
-    pinMode(1, INPUT_PULLUP); 
+void setup() {
+    // Sensor 1 initializing
+    pinMode(sensorOnePin, INPUT_PULLUP); // INPUT_PULLUP -> sensor HIGH when triggered. 
 
-    // Sensor 2 
-    pinMode(2, INPUT_PULLUP); //INPUT_PULLUP internally connects the pin to a HIGH logic level. 
-    
-    // Button 
-    pinMode(3, INPUT_PULLUP); 
+    // Sensor 2 initializing
+    pinMode(sensorTwoPin, INPUT_PULLUP);
 
-    // Initialize communication protocol between external digital timer here ()
-    lcd.begin(16, 2); // LCD rows/columns 
-    lcd.clear(); 
-    lcd.print("On your mark, get set..."); //Initial message 
-    lcd.display(): 
+    // Enable dots in time (ex: 11:11)
+    drawDots = true; 
 
-    Serial.begin(9600); //start serial connection
+  // I2C communication port for LED display 
+    matrix.begin(0x70);
+
+    // Initializing each digit on LED display to 0 
+    dig0 = 0, dig1=0, dig3=0, dig4 = 0; 
+
+    // State of timer. Can be 0 (00:00), 1 (timer running), or 2 (Timer stopped).
+    state = 0;
+
+    // Serial monitor for debugging. Can delete later. 
+    Serial.begin(9600);
 }
 
-void loop(){
+void loop() {
+    // Printing state of timer for debugging. Can delete later. 
+    Serial.println(state);
 
-    lcd.setCursor(0, 1); // Set cursor to column 0, row 1 
-
-    if(digitalRead(sensorOnePin) == LOW && !firstSensorTriggered){ // If the first sensor is triggered for the first time, 
-        firstSensorTriggered = true; // mark as triggered
-        timerRunning = true; // Set timer running equal to true 
-        startTime = millis(); // Start timer 
-        delay(400);
-        
-        // Send signal to external timer here using timer modules 
+    if(digitalRead(sensorOnePin) == HIGH){ // If the first sensor is triggered, 
+      delay(500); // Short delay. WILL NOT WORK without delay. Can play around with this number, but it gets buggy without a short delay because it triggers the button multiple times in one click.
+      if(state==2){ // If the state is 2 and the button was clicked, reset the state to 0 (to display 00:00).
+        state=0;
+      }else{ // If the sensor is triggered, increase the state value.
+        state++;
+      }
     }
 
-    if(timerRunning == true){
-        lcd.setCursor(0, 1);
-        lcd.print((millis()- startTime) / 1000.0); // Outputting # of seconds elapsed since beginning 
+      if(state==0){ // If the state is 0, display (00:00).
+        resetTimer(); 
+      }else if(state==1){ // If the state is 1, enter function with the for-loop for the timer. Stays here in this function until the sensor is triggered again, in which case the
+      // state is increased again. 
+        drawTime();
+      } else{ // If the state is 2, displays the values stored in dig0dig1 : dig3dig4. These values store the most current value displayed on the timer. 
+        displayEndTime();
+      }
+}
+
+// Stopwatch function. If the timer is in state 0 (00:00) and the sensor is triggered, the program will trigger this function. 
+void drawTime(){
+  Serial.println(state); // For debugging. Can delete later. 
+    for (uint16_t counter = 0; counter < 9999; counter ++) {
+      // Storing the values of each digit on the LED display 
+      dig0=counter/1000; 
+      dig1 = counter/100; 
+      dig3= counter/10;
+      dig4=counter%10;
+
+      // Writing the digits to the LED display 
+      matrix.writeDigitNum(0, (counter / 1000), drawDots);
+      matrix.writeDigitNum(1, (counter / 100) % 10, drawDots);
+      matrix.drawColon(drawDots);
+      matrix.writeDigitNum(3, (counter / 10) % 10, drawDots);
+      matrix.writeDigitNum(4, counter % 10, drawDots);
+      matrix.writeDisplay();
+      delay(8); // This statement is needed or else the timer goes insanely fast. You can play around with this as necessary. However, I found that the 8ms delay 
+      // made the loop match up closely with an actual stopwatch. 
+
+      // If the sensor is triggered while the stopwatch is runing, increase the state to 2 to stop the numbers and display the most recent digits. Then break out of the loop. 
+      if(digitalRead(sensorOnePin) == HIGH) {
+          state++;
+          delay(1000); // Again, this delay is necessary to avoid buginess but the exact value can probably be played around with.
+          break;
+      }
     }
+}
 
-    // If second sensor triggered and the first one was triggered, calculate elapsed time. 
-    if(digitalRead(sensorTwoPin) == LOW && firstSensorTriggered) {
-        endTime = millis(); 
-        timerRunning = false; // Set timer running to false 
-        elapsedTime = endTime - startTime; 
-        delay(100);
-        // Stop counting & display elapsed time on timer here 
-    }
+// When the state is equal to 2 (ie the sensor was triggered while the stopwatch was running), display the digits stored in dig0dig1:dig3dig4 (freeze the timer).
+void displayEndTime(){
+    matrix.writeDigitNum(0, dig0, drawDots);
+    matrix.writeDigitNum(1, dig1, drawDots);
+    matrix.drawColon(drawDots);
+    matrix.writeDigitNum(3, dig3, drawDots);
+    matrix.writeDigitNum(4, dig4, drawDots);
+    matrix.writeDisplay();
+}
 
-    // If the button is pressed, reset start/end time and firstSensorTriggered. 
-    if(digitalRead(buttonPin) == LOW){
-        startTime = 0; 
-        endTime = 0; 
-        firstSensorTriggered = false; 
-        timerRunning = false;
-        delay(100); // make sure to not trigger it twice. Small delay. 
-        
-        // Set timer equal to 0 
-    }
-
-    // May need to format time 
-
+// When the state is equal to 0, display 00:00. This is the default state and will also display 00:00 after the reset button is triggered, 
+void resetTimer(){
+  matrix.writeDigitNum(0, 0, drawDots);
+  matrix.writeDigitNum(1, 0, drawDots);
+  matrix.writeDigitNum(3, 0, drawDots);
+  matrix.writeDigitNum(4, 0, drawDots);
+  matrix.writeDisplay();
 }
